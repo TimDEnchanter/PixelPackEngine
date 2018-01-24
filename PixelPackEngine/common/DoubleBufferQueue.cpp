58c50081c10e0421aaa72e512dad68d421787a59
Timothy McCabe <mcmit1234@gmail.com>
2018-01-24 13:46:29 -0500

    further implementation of buffer queue

--- PixelPackEngine/common/DoubleBufferQueue.cpp
+++ PixelPackEngine/common/DoubleBufferQueue.cpp
@@ -1,14 +1,22 @@
 #include "DoubleBufferQueue.h"
 
-void pxpk::DoubleBuffferQueue::write(pxpk::QueueEvent)
+void pxpk::DoubleBuffferQueue::write(pxpk::QueueEvent input)
 {
+	std::lock_guard<std::mutex> lock(queue_mutex);
+	writeQueue.push(input);
 }
 
 pxpk::QueueEvent pxpk::DoubleBuffferQueue::read()
 {
-	return pxpk::QueueEvent();
+	pxpk::QueueEvent out = readQueue.front();
+	readQueue.pop();
+	return out;
 }
 
 void pxpk::DoubleBuffferQueue::swap()
 {
+	//clear read queue
+	std::queue<pxpk::QueueEvent>().swap(readQueue);
+	//swap queues
+	std::swap(writeQueue, readQueue);
 }
--- PixelPackEngine/common/DoubleBufferQueue.h
+++ PixelPackEngine/common/DoubleBufferQueue.h
@@ -2,6 +2,7 @@
 #define DOUBLE_BUFFER_QUEUE_H
 
 #include <queue>
+#include <mutex>
 
 #include "QueueEvent.h"
 
@@ -12,6 +13,8 @@ namespace pxpk
 		private:
 			std::queue<pxpk::QueueEvent> writeQueue;
 			std::queue<pxpk::QueueEvent> readQueue;
+
+			std::mutex queue_mutex;
 
 		public:
 			void write(pxpk::QueueEvent);
